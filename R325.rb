9de581baecc5dc00d80080db04388d787f359782
JDeanThomas <JDeanThomas#gmail.com>
2018-03-01 02:10:43 -0500

    Backup

---
+++ R325.rb
@@ -0,0 +1,289 @@
+class RDownloadStrategy < SubversionDownloadStrategy
+  def stage
+    cp_r File.join(cached_location, "."), Dir.pwd
+  end
+end
+
+class R325 < Formula
+  desc "Software environment for statistical computing"
+  homepage "http://www.r-project.org/"
+  url "http://cran.rstudio.com/src/base/R-3/R-3.2.5.tar.gz"
+  mirror "http://cran.r-project.org/src/base/R-3/R-3.2.5.tar.gz"
+  sha256 "60745672dce5ddc201806fa59f6d4e0ba6554d8ed78d0f9f0d79a629978f80b5"
+
+  # Do not remove executable permission from these scripts.
+  # See https://github.com/Linuxbrew/linuxbrew/issues/614
+  skip_clean "lib/R/bin" unless OS.mac?
+
+  bottle do
+    sha256 "3a6c10598d99a6be0565e938d230ffd3b1acc5f56e85e13b82fbda6cfcc6cba9" => :el_capitan
+    sha256 "b38dfeae6629196a31320372521604d5e8a7a99ad0d451d55c9a30a09f1c38b4" => :yosemite
+    sha256 "6a352a54dab8d0cb25d154fd463e8caf380fc2b837ee658a186052fd6aad77af" => :mavericks
+  end
+
+  head do
+    url "https://svn.r-project.org/R/trunk", :using => RDownloadStrategy
+    depends_on :tex
+  end
+
+  option "without-shared-BLAS", "Build BLAS as a static library"
+  option "without-accelerate", "Build without the Accelerate framework (use Rblas)"
+  option "without-test", "Skip build-time tests (not recommended)"
+  option "without-tcltk", "Build without Tcl/Tk"
+  option "with-librmath-only", "Only build standalone libRmath library"
+  option "with-llvm-clang", "Compile using llvm Clang instead of Apple Clang"
+  option "with-clang-omp", "Compile using OpenMP extension library for Clang"
+
+  depends_on "pkg-config" => :build
+  depends_on "texinfo" => :build
+  depends_on :fortran
+  depends_on "readline"
+  depends_on "gettext"
+  depends_on "libtiff"
+  depends_on "pcre"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "xz"
+  depends_on "curl" unless OS.mac?
+
+  depends_on "openblas" => :optional
+  depends_on "pango" => :optional
+  depends_on "valgrind" => :optional
+  depends_on :x11 => (OS.mac? ? :optional : :recommended)
+  
+  cairo_opts = build.with?("x11") ? ["with-x11"] : []
+  cairo_opts << :optional if OS.linux?
+  depends_on "cairo" => cairo_opts
+
+  patch :DATA  
+  
+  def install
+    # Fix dyld: lazy symbol binding failed: Symbol not found: _clock_gettime
+    if MacOS.version == "10.11" && MacOS::Xcode.installed? && MacOS::Xcode.version >= "8.0"
+      ENV["ac_cv_have_decl_clock_gettime"] = "no"
+    end
+      
+    # Fix cairo detection with Quartz-only cairo
+    inreplace ["configure", "m4/cairo.m4"], "cairo-xlib.h", "cairo.h"
+
+    args = [
+      "--prefix=#{prefix}",
+      "--with-libintl-prefix=#{Formula["gettext"].opt_prefix}",
+      "--enable-memory-profiling",
+      "—with-system-zlib",
+    ]
+    
+    # don't remember Homebrew's sed shim
+    args << "SED=/usr/bin/sed" if File.exist?("/usr/bin/sed")
+    
+    if OS.linux?
+      args << "--libdir=#{lib}" # avoid using lib64 on CentOS
+      args << "--enable-R-shlib"
+      # If LDFLAGS contains any -L options, configure sets LD_LIBRARY_PATH to
+      # search those directories. Remove -LHOMEBREW_PREFIX/lib from LDFLAGS.
+      ENV.remove "LDFLAGS", "-L#{HOMEBREW_PREFIX}/lib"
+    else
+      args << "--enable-R-framework"
+      args << "--with-cairo"
+
+    
+    if ENV.compiler == /^clang-3.8/
+      puts ENV.cc
+      puts ENV.compiler 
+      abort("Tiiiiiiiight!")
+    end
+    
+    # Set global compiler deafualts if using Clang or GNU GCC
+    if ENV.compiler =~ /^gcc-/ || ENV.compiler == :clang || ENV.compiler == /^clang-/
+      # Set GCC gfortran as fortran compiler
+      ENV["FC"] = "gfortran"
+      ENV["F77"] = "gfortran"  
+      # Add Fortran compiler flags to enviroment 
+      ENV["FCFLAGS"] = " -Wall -march=native -g -O2"
+      ENV["FFLAGS"] = "-Wall -march=native -g -O2"
+      # Add C/C++ compiler flags to the environment
+      ENV.append_to_cflags "-Wall -march=native -g -O2"
+    # Fall back to defaults of compiler if neither Clang
+    # nor GNU GCC. Pass argument --env-std to use non-Homebrew
+    # (standard) environment as pass appropriate args for build
+    end
+    
+    # Additional compliler specific settings
+    # If using GNU (non-Apple) GCC version
+    if ENV.compiler =~ /^gcc-/
+      # Set Clang (Xcode tools) for ObjectiveC. Apple is standard.
+      ENV["OBJC"] = "clang"
+      ENV["OBJCXX"] = "clang++"  
+    # Arguments for LLVM (non-Apple) Clang
+    elsif build.with? "llvm-clang"
+      ENV["CC"] = "clang-3.8"
+      ENV["CXX"] = "clang-3.8++"
+      
+    # Arguments for built in (Apple) Clang with Clang-OMP 
+    # C/C++ external library extensions (based on Clang 3.5) 
+    elsif build.with? "clang-omp" 
+      # Set C/C++ to OpenMP enabled Clang extansion
+      ENV["CC"] = "clang-omp"
+      ENV["CXX"] = "clang-omp++"
+      # Add aditional OpenMP compiler dirrectives
+      ENV.append "DYLIB_LDFLAGS", "-fopenmp" 
+      ENV.append "MAIN_LDFLAGS ", "-fopenmp"
+      #ENV.append "SHLIB_OPENMP_CFLAGS", "-fopenmp"
+      #ENV.append "SHLIB_OPENMP_CXXFLAGS", "-fopenmp"
+      ENV.append "SHLIB_OPENMP_FCFLAGS", "-fopenmp"
+      ENV.append "SHLIB_OPENMP_FFLAGS", "-fopenmp"
+    # Fallback to global defaults for standard Clang    
+    end
+      
+
+      # Still not fixed as of GCC 6.1
+    
+      # Disable building against the Aqua framework with CLT >= 6.0.
+      # See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=63651
+      # This should be revisited when new versions of GCC come along.
+      if ENV.compiler != :clang && MacOS::CLT.version >= "6.0"
+        args << "--without-aqua"
+      else
+        args << "--with-aqua"
+      end
+    end
+
+    if build.with? "valgrind"
+      args << "--with-valgrind-instrumentation=2"
+      ENV.Og
+    end
+
+    if build.with? "openblas"
+      args << "--with-blas=-L#{Formula["openblas"].opt_lib} -lopenblas" << "--with-lapack"
+      ENV.append "LDFLAGS", "-L#{Formula["openblas"].opt_lib}"
+    elsif build.with? "accelerate"
+      args << "--with-blas=-framework Accelerate" << "--with-lapack"
+      ENV.append_to_cflags "-D__ACCELERATE__" if ENV.compiler != :clang
+      # Defualt to BLAS as shared lib. R Binary standard for Rblas.
+      args << "--enable-BLAS-shlib" if build.with? "shared-BLAS"
+      # Fall back to Rblas without-accelerate or -openblas
+    end
+
+    args << "--without-tcltk" if build.without? "tcltk"
+    args << "--without-x" if build.without? "x11"
+
+    # Help CRAN packages find gettext, readline, and openssl
+    %w[gettext readline openssl].each do |f|
+      ENV.append "CPPFLAGS", "-I#{Formula[f].opt_include}"
+      ENV.append "LDFLAGS", "-L#{Formula[f].opt_lib}"
+    end
+
+    # Pull down recommended packages if building from HEAD.
+    system "./tools/rsync-recommended" if build.head?
+
+    system "./configure", *args
+
+    if build.without? "librmath-only"
+      system "make"
+      ENV.deparallelize # Serialized installs, please
+      system "make check 2>&1 | tee make-check.log" if build.with? "check"
+      system "make", "install"
+
+      # Link binaries, headers, libraries, & manpages from the Framework
+      # into the normal locations
+      if OS.mac?
+        bin.install_symlink prefix/"R.framework/Resources/bin/R"
+        bin.install_symlink prefix/"R.framework/Resources/bin/Rscript"
+        frameworks.install_symlink prefix/"R.framework"
+        include.install_symlink Dir[prefix/"R.framework/Resources/include/*"]
+        lib.install_symlink prefix/"R.framework/Resources/lib/libR.dylib"
+        man1.install_symlink prefix/"R.framework/Resources/man1/R.1"
+        man1.install_symlink prefix/"R.framework/Resources/man1/Rscript.1"
+      end
+
+      # if this was built with a Homebrew gfortran, immunize to minor gcc version changes
+      if (r_home/"etc/Makeconf").read.include? Formula["gcc"].prefix
+        inreplace r_home/"etc/Makeconf", Formula["gcc"].prefix, Formula["gcc"].opt_prefix
+      end
+
+      # make Homebrew packages discoverable for R CMD INSTALL
+      inreplace r_home/"etc/Makeconf" do |s|
+        s.gsub! /^CPPFLAGS =.*/, "\\0 -I#{HOMEBREW_PREFIX}/include"
+        s.gsub! /^LDFLAGS =.*/, "\\0 -L#{HOMEBREW_PREFIX}/lib"
+        s.gsub! /.LDFLAGS =.*/, "\\0 $(LDFLAGS)"
+      end
+
+      #bash_completion.install resource("completion")
+
+      prefix.install "make-check.log" if build.with? "check"
+    end
+
+    cd "src/nmath/standalone" do
+      system "make"
+      ENV.deparallelize # Serialized installs, please
+      system "make", "install"
+
+      if OS.mac?
+        lib.install_symlink Dir[prefix/"R.framework/Versions/[0-9]*/Resources/lib/libRmath.dylib"]
+        include.install_symlink Dir[prefix/"R.framework/Versions/[0-9]*/Resources/include/Rmath.h"]
+      end
+    end
+  end
+
+  def post_install
+    return if build.with?("librmath-only")
+    cellar_site_library = r_home/"site-library"
+    site_library.mkpath
+    cellar_site_library.unlink if cellar_site_library.exist? || cellar_site_library.symlink?
+    ln_s site_library, cellar_site_library
+  end
+
+  def caveats
+    if build.without? "librmath-only" then <<-EOS.undent
+      To enable rJava support, run the following command:
+        R CMD javareconf JAVA_CPPFLAGS=-I/System/Library/Frameworks/JavaVM.framework/Headers
+      If you've installed a version of Java other than the default, you might need to instead use:
+        R CMD javareconf JAVA_CPPFLAGS="-I/System/Library/Frameworks/JavaVM.framework/Headers -I/Library/Java/JavaVirtualMachines/jdk<version>.jdk/"
+      (where <version> can be found by running `java -version`, `/usr/libexec/java#{'_'}home`, or `locate jni.h`), or:
+        R CMD javareconf JAVA_CPPFLAGS="-I/System/Library/Frameworks/JavaVM.framework/Headers -I$(/usr/libexec/java#{'_'}home | grep -o '.*jdk')"
+      EOS
+    end
+  end
+
+  test do
+    if build.without? "librmath-only"
+      system bin/"Rscript", "-e", "print(1+1)"
+      system bin/"Rscript", "-e", "quit('no', capabilities('cairo')[['cairo']] != TRUE)" if OS.mac?
+    end
+  end
+
+  def installed_short_version
+    old_rhome = ENV.delete "R_HOME" # Rscript prints garbage if R_HOME is set
+    `#{bin}/Rscript -e 'cat(as.character(getRversion()[1,1:2]))'`.strip
+  ensure
+    ENV["R_HOME"] = old_rhome
+  end
+
+  def r_home
+    OS.mac? ? (prefix/"R.framework/Resources") : (prefix/"lib/R")
+  end
+
+  def site_library
+    HOMEBREW_PREFIX/"lib/R/#{installed_short_version}/site-library"
+  end
+end
+
+__END__
+diff --git a/src/modules/lapack/vecLibg95c.c b/src/modules/lapack/vecLibg95c.c
+index ffc18e4..6728244 100644
+--- a/src/modules/lapack/vecLibg95c.c
++++ b/src/modules/lapack/vecLibg95c.c
+@@ -2,6 +2,12 @@
+ #include <config.h>
+ #endif
+
++#ifndef __has_extension
++#define __has_extension(x) 0
++#endif
++#define vImage_Utilities_h
++#define vImage_CVUtilities_h
++
+ #include <AvailabilityMacros.h> /* for MAC_OS_X_VERSION_10_* -- present on 10.2+ (according to Apple) */
+ /* Since OS X 10.8 vecLib requires Accelerate to be included first (which in turn includes vecLib) */
+ #if defined MAC_OS_X_VERSION_10_8 && MAC_OS_X_VERSION_MIN_REQUIRED >= 1040
+    
